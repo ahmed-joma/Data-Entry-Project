fe8c15bcb59d57e6d84bc58818a944ff245fe1d8
Ahmed Joma <ahmedjomma18@gmail.com>
2026-01-03 23:01:51 +0100

    Refactor app.js to use EJS for rendering views, add user routes, and remove home.html; update package.json and package-lock.json to include EJS and livereload dependencies.

--- app.js
+++ app.js
@@ -4,20 +4,36 @@ const port = 3000
 const mongoose = require('mongoose');
 app.use(express.urlencoded({ extended: true })); 
 const Mydata = require('./models/mydataschema');
+app.set('view engine', 'ejs'); 
+app.use(express.static('public'));
+
+//routes
 app.get('/', (req, res) => {
-    res.sendFile( '/views/home.html', { root: __dirname })
-  })
-  app.post('/Submit', (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const mydata = new Mydata({ name, email });
-    mydata.save().then(() => {
-        res.send('Data saved successfully');
+    Mydata.find().then((result) => {
+        res.render("index",{});
     }).catch((err) => {
-        console.error('Error saving data:', err);
-        res.status(500).send('Error saving data');
+        console.error('Error fetching data:', err);
+        res.status(500).send('Error fetching data');
     }) ;
   })
+  app.get('/user/add.html', (req, res) => {
+    res.render("user/add",{});
+  })
+  app.get('/user/view.html', (req, res) => {
+    res.render("user/view",{});
+  })
+  app.get('/user/edit.html', (req, res) => {
+    res.render("user/edit",{});
+  })
+  app.get('/user/search.html', (req, res) => {
+    res.render("user/search",{});
+  })
+  app.get('/user/view.html', (req, res) => {
+    res.render("user/view",{});
+  })
+
+
+  //connect to database
 
 mongoose.connect('mongodb+srv://ahmedjomma18:jX11C4KDhXHvlR6w@cluster0.1xatthb.mongodb.net/?appName=Cluster0').then(() => {
     app.listen(port, () => {
